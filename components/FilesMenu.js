df65984bbdacebff561b4e892477a676f0fa3b35
unknown <essidmohamedamin@gmail.com>
2021-08-18 19:32:20 +0100

    display text if no files

--- components/FilesMenu.js
+++ components/FilesMenu.js
@@ -3,8 +3,17 @@ import FileCard from "./FileCard";
 
 export default function FilesMenu({ filtredFiles }) {
   console.log(filtredFiles);
-  const FilesCards = filtredFiles.map((file, index) => (
-    <FileCard key={file._id} file={file} />
-  ));
+  let FilesCards;
+  if (filtredFiles.length > 0) {
+    FilesCards = filtredFiles.map((file, index) => (
+      <FileCard key={file._id} file={file} />
+    ));
+  } else {
+    FilesCards = (
+      <div style={{ color: "black", fontSize: "24px" }}>
+        هذه الوثائق لا تتوفر حاليا، انتظرنا في القريب العاجل
+      </div>
+    );
+  }
   return <div className={styles.filesContainer}>{FilesCards}</div>;
 }
